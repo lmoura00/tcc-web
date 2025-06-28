19d70cc3cce0efe768a3d90a72a13e1f69345a37
Lucas Moura <lucasmoura53@gmail.com>
2025-06-27 23:36:30 -0300

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,24 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import {defineConfig} from "eslint/config";
-
-const baseRules = {
-	rules: {
-		eqeqeq: "off",
-		"no-unused-vars": "error",
-		"prefer-const": ["error", { ignoreReadBeforeAssign: true }],
-	},
-};
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
-
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  baseRules,
+  ...compat.config({
+    extends: ["next", 'next/core-web-vitals', 'next/typescript'],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
